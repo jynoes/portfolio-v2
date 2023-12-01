de465db6658c4601d49c8693c812c7dce62f6897
jynoes <89066947+jynoes@users.noreply.github.com>
2023-12-01 21:23:38 +0800

    Update Botbar.jsx

--- src/components/Botbar.jsx
+++ src/components/Botbar.jsx
@@ -179,6 +179,7 @@ const Botbar = ({ activeSection }) => {
         <Container id="botbar-playground">
           
           <Row>Please update this</Row><Row>test I have to add a Pinterest style menu here</Row>
+          <Row>Please update this</Row><Row>test I have to add a Pinterest style menu here</Row>
         </Container>
       )}
 
