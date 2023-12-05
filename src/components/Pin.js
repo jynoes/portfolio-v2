8b071e942575e99140279d44589cf8f457dfa595
jynoes <89066947+jynoes@users.noreply.github.com>
2023-12-05 14:58:10 +0800

    tried pinterest style

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
-const Pin = () => {
+function Pin(props) {
   return (
-    <div>Pin</div>
+    <Container >
+    <div style={{
+      ...styles.pin,
+      ...styles[props.size]
+    }}>
+
+    </div>
+    </Container>
   )
+}
+
+const styles = {
+  pin: {
+    margin: '15px 10px',
+    padding: 0,
+    borderRadius: '16px',
+    backgroundColor: 'red'
+  },
+  small: {
+    gridRowEnd: 'span 26'
+  },
+  medium: {
+    gridRowEnd: 'span 33'
+  },
+  large: {
+    gridRowEnd: 'span 45'
+  },
 }
 
 export default Pin
--- src/components/PinterestLayout.js
+++ src/components/PinterestLayout.js
@@ -4,14 +4,25 @@ import Pin from './Pin'
 const PinterestLayout = () => {
   return (
     <div style={styles.pin_container}>
-        PinterestLayout
+        <Pin size='small' />
+        <Pin size='medium' />
+        <Pin size='large' />
     </div>
   )
 }
 
 const styles = {
     pin_container: {
-
+        margin: 0,
+        padding: 0,
+        width: '80vw',
+        backgroundColor: 'black',
+        position: 'absolute',
+        left: '50',
+        transform: 'translateX(-50%)',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, 250px)',
+        gridAutoRows: '10px',
     }
 }
 
