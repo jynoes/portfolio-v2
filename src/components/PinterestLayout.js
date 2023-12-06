17921b5f6e923ef12ced2ccb34ac6ef9b8537aba
jynoes <89066947+jynoes@users.noreply.github.com>
2023-12-06 18:56:55 +0800

    update

--- src/components/PinterestLayout.js
+++ src/components/PinterestLayout.js
@@ -1,33 +1,33 @@
-import React from 'react'
-import Pin from './Pin'
-import { Container } from 'react-bootstrap'
+import React from "react";
+import Pin from "./Pin";
+import { Container } from "react-bootstrap";
 
 function PinterestLayout() {
   return (
     <Container>
-    <div style={styles.pin_container}>
-        <Pin size='small' />
-        <Pin size='medium' />
-        <Pin size='large' />
-    </div>
+      <div style={styles.pin_container}>
+        <Pin size="small" />
+        <Pin size="medium" />
+        <Pin size="large" />
+      </div>
     </Container>
-  )
+  );
 }
 
 const styles = {
-    pin_container: {
-        margin: 0,
-        padding: 0,
-        width: '80vw',
-        position: 'absolute',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fill, 250px)',
-        gridAutoRows: '10px',
-        justifyContent: 'center',
-        backgroundColor: 'black',
-    }
-}
+  pin_container: {
+    margin: 0,
+    padding: 0,
+    width: "80vw",
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, 250px)",
+    gridAutoRows: "10px",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+};
 
-export default PinterestLayout
+export default PinterestLayout;
