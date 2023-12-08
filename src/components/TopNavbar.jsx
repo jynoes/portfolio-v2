711b943a3a374e0edd6c20bc435c3ccbdc899a50
jynoes <89066947+jynoes@users.noreply.github.com>
2023-12-08 21:45:28 +0800

    update topnavbar.jsx

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,37 +1,65 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import { Col } from "react-bootstrap";
 import "../styles/Topbar.css";
 
-const titles = ["summertonic", "simple", "steady"]
-
+const words = ['summertonic', 'transition', 'another', 'word', 'example'];
 
 const TopNavbar = ({ onResetClick }) => {
-  const [activeSection, setActiveSection] = useState('Home');
+  const [activeSection, setActiveSection] = useState("Home");
+
+  const [transitionWord, setTransitionWord] = useState(words[0]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, 2000); // Interval duration in milliseconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    setTransitionWord(words[currentIndex]);
+  }, [currentIndex]);
 
   return (
     <Navbar expand="lg" className="top-navbar">
-        <Container className="navbar-container">
-          <Col lg={4} className="topbar-1">
-            Excited for 
-            <span className="bolder"> Designer &<br />Developer </span>Positions
-          </Col>
+      <Container className="navbar-container">
+        <Col lg={4} className="topbar-1">
+          Excited for
+          <span className="bolder">
+            {" "}
+            Designer &<br />
+            Developer{" "}
+          </span>
+          Positions
+        </Col>
 
-          <Col lg={4}>
-            <Navbar.Brand id="main-brand" className="topbar-2" onClick={() => { setActiveSection("Home"); onResetClick(); }}>
-              summertonic
-            </Navbar.Brand>
-          </Col>
+        <Col lg={4}>
+          <Navbar.Brand
+            id="main-brand"
+            className="topbar-2"
+            onClick={() => {
+              setActiveSection("Home");
+              onResetClick();
+            }}
+          >
+            {transitionWord}
+          </Navbar.Brand>
+        </Col>
 
-          <Col lg={4}>
-            <Container className="topbar-3">
-              Portfolio <br /> — Volume 2
-            </Container>
-          </Col>
-        </Container>
-      </Navbar>
-  )
-}
+        <Col lg={4}>
+          <Container className="topbar-3">
+            Portfolio <br /> — Volume 2
+          </Container>
+        </Col>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default TopNavbar
+export default TopNavbar;
