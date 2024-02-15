fe970c93c213b549ea31719bf7a0e2c747bca627
jynoes <89066947+jynoes@users.noreply.github.com>
2024-02-16 00:19:56 +0800

    Update pinterest layout

--- src/components/PinterestLayout.js
+++ src/components/PinterestLayout.js
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 
 function PinterestLayout() {
   return (
+    <div style={styles.scrollableContainer}>
       <Container style={styles.pin_container}>
         <Pin size="small" />
         <Pin size="medium" />
@@ -15,10 +16,15 @@ function PinterestLayout() {
         <Pin size="medium" />
         <Pin size="large" />
       </Container>
+    </div>
   );
 }
 
 const styles = {
+  scrollableContainer: {
+    height: "100vh", // Set the height to 100% of viewport height
+    overflowY: "auto", // Enable vertical scrolling
+  },
   pin_container: {
     margin: 0,
     padding: 0,
