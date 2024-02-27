c6b75033efc5e46b139a4310cdf49adf52ee5a75
jynoes <89066947+jynoes@users.noreply.github.com>
2024-02-27 17:51:10 +0800

    Revert back

--- src/components/PinterestLayout.js
+++ src/components/PinterestLayout.js
@@ -43,40 +43,6 @@ function PinterestLayout() {
   );
 }
 
-// const styles = {
-//   pin_container: {
-//     margin: 0,
-//     padding: 0,
-//     marginTop: "-10px",
-//     width: "100%",
-//     position: "relative",
-//     left: "50%",
-//     transform: "translateX(-50%)",
-//     display: "grid",
-//     gridTemplateColumns: "repeat(auto-fill, 250px)",
-//     gridAutoRows: "10px",
-//     justifyContent: "space-between",
-//   },
-
-// };
-
-// const styles = {
-//   pin_container: {
-//     margin: 0,
-//     padding: 0,
-//     marginTop: "-10px",
-//     width: "100%",
-//     position: "relative",
-//     left: "50%",
-//     transform: "translateX(-50%)",
-//     display: "grid",
-//     gridAutoRows: "10px",
-//     justifyContent: "space-between",
-//     gridGap: "10px", // Add some gap between the pins
-//     gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))", // Base column width of 250px
-//   },
-// };
-
 const styles = {
   pin_container: {
     margin: 0,
@@ -87,17 +53,13 @@ const styles = {
     left: "50%",
     transform: "translateX(-50%)",
     display: "grid",
-    gridAutoRows: "auto", // Allow rows to size based on content
+    gridTemplateColumns: "repeat(auto-fill, 250px)",
+    gridAutoRows: "10px",
     justifyContent: "space-between",
-    gridGap: "10px", // Add some gap between the pins
-    gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))", // Adjust the min and max widths
   },
 
-  pin: {
-    aspectRatio: "1 / 1", // Maintain a 1:1 aspect ratio for each pin container
-    width: "100%", // Ensure the width of the pin container fills the grid column
-  },
 };
+
 
 
 export default PinterestLayout;
